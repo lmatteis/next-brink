5c9680e4dfd22566d96fdd7971cd8b252b643f4d
Luca Matteis <lmatteis@gmail.com>
2023-05-21 12:04:59 +0200

    Add second section with grid-like flexbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
 export default function Home() {
   return (
-    <div className="flex overflow-auto gap-10">
-      <div className="h-20 bg-cold-blue w-[85vw] shrink-0">ciao</div>
-      <div className="h-20 bg-cold-blue w-[85vw] shrink-0">ciao</div>
+    <div>
+      <div className="flex overflow-auto gap-2 h-32">
+        <div className="bg-cold-blue w-[85vw] lg:w-auto lg:flex-1 shrink-0">
+          ciao
+        </div>
+        <div className="bg-cold-blue w-[85vw] lg:w-auto lg:flex-1 shrink-0">
+          ciao
+        </div>
+      </div>
+      <h2>Ciao</h2>
+      <div className="flex gap-2 h-80 overflow-auto">
+        <div className="flex gap-2 flex-row lg:flex-col flex-1">
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">1</div>
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">2</div>
+        </div>
+        <div className="flex gap-2 flex-row lg:flex-col flex-1">
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">3</div>
+        </div>
+        <div className="flex gap-2 flex-row lg:flex-col flex-1">
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">4</div>
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">5</div>
+        </div>
+      </div>
     </div>
   );
 }
