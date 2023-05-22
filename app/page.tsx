02c88c1472cbaa3341acfdd6fcd09ce131ada31b
Luca Matteis <lmatteis@gmail.com>
2023-05-22 20:56:12 +0200

    Adjust padding of second section

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
           Utrum ut placerat nec, varius sit amet lacus.
         </div>
       </div>
-      <div className="flex overflow-auto gap-2 h-[776px]">
+      <div className="flex overflow-auto gap-5 lg:gap-10 h-[776px]">
         <div className="bg-cold-blue w-[85vw] lg:w-auto lg:flex-1 shrink-0"></div>
         <div className="bg-cold-blue w-[85vw] lg:w-auto lg:flex-1 shrink-0"></div>
       </div>
@@ -28,20 +28,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex gap-2 h-80 overflow-auto">
-        <div className="flex gap-2 flex-row lg:flex-col flex-1">
-          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">1</div>
-          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">2</div>
+
+      <div className="flex gap-5 lg:gap-10 h-[620px] overflow-auto mx-0 lg:mx-[20px] mb-[100px]">
+        <div className="flex gap-5 lg:gap-10 flex-row lg:flex-col flex-1">
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto"></div>
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto"></div>
         </div>
-        <div className="flex gap-2 flex-row lg:flex-col flex-1">
-          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">3</div>
+        <div className="flex gap-5 lg:gap-10 flex-row lg:flex-col flex-1">
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto"></div>
         </div>
-        <div className="flex gap-2 flex-row lg:flex-col flex-1">
-          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">4</div>
-          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto">5</div>
+        <div className="flex gap-5 lg:gap-10 flex-row lg:flex-col flex-1">
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto"></div>
+          <div className="bg-cold-blue flex-1 w-[85vw] lg:w-auto"></div>
         </div>
       </div>
-      <h2>Ciao</h2>
 
       <div className="bg-black text-white">
         <div className="flex gap-2 overflow-auto flex-col pt-[100px]">
